cfca9b39303903ebe98ff58f6a261491deb7d71e
JadonF <jadon.fournier@ocsbstudent.ca>
2018-04-03 09:58:53 -0400

    Initial commit

---
+++ PerfectSquares/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PerfectSquares
+{
+    partial class PerfectSquares
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.nudMaxValue = new System.Windows.Forms.NumericUpDown();
+            this.lstPerfectSquaresList = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMaxValue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(151, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(120, 42);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // nudMaxValue
+            // 
+            this.nudMaxValue.Location = new System.Drawing.Point(151, 76);
+            this.nudMaxValue.Name = "nudMaxValue";
+            this.nudMaxValue.Size = new System.Drawing.Size(120, 20);
+            this.nudMaxValue.TabIndex = 1;
+            // 
+            // lstPerfectSquaresList
+            // 
+            this.lstPerfectSquaresList.FormattingEnabled = true;
+            this.lstPerfectSquaresList.Location = new System.Drawing.Point(99, 129);
+            this.lstPerfectSquaresList.Name = "lstPerfectSquaresList";
+            this.lstPerfectSquaresList.Size = new System.Drawing.Size(216, 95);
+            this.lstPerfectSquaresList.TabIndex = 2;
+            // 
+            // PerfectSquares
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 280);
+            this.Controls.Add(this.lstPerfectSquaresList);
+            this.Controls.Add(this.nudMaxValue);
+            this.Controls.Add(this.btnStart);
+            this.Name = "PerfectSquares";
+            this.Text = "Perfect Squares";
+            ((System.ComponentModel.ISupportInitialize)(this.nudMaxValue)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NumericUpDown nudMaxValue;
+        private System.Windows.Forms.ListBox lstPerfectSquaresList;
+    }
+}
+
---
+++ PerfectSquares/Form1.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * Created by: Jadon Fournier
+ * Created on: 3-Mar-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #21 - Perfect Squares
+ * This program calculates which numbers are perfect squares below a user inputted value.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PerfectSquares
+{
+    public partial class PerfectSquares : Form
+    {
+        public PerfectSquares()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Define variables
+            int MaxValue;
+            int Value = 1;
+            double RootDouble;
+            int RootInt;
+
+            //Clear list
+            this.lstPerfectSquaresList.Items.Clear();
+
+            //Get the max value from user
+            MaxValue = Convert.ToInt32(nudMaxValue.Value);
+
+            //Use a while loop to check it
+            while (Value <= MaxValue)
+            {
+                //Take square root of value
+                RootDouble = Math.Sqrt(Value);
+
+                //Convert the root as a double to an int and assign it to a variable
+                RootInt = Convert.ToInt32(RootDouble);
+
+                if (RootDouble == RootInt)
+                {
+                    //add the lsit item
+                    this.lstPerfectSquaresList.Items.Add(Value + " is a perfect square.");
+
+                    //Refresh the form
+                    this.Refresh();
+                }
+                Value = Value + 1;
+            }
+            
+        }
+    }
+}
